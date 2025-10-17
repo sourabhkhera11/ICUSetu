83fd45bf36d15bd502499a43ef7e9cf19611fedf
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-17 19:50:20 +0530

    Restructuting the app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,36 @@
-//Setting up the express
+//Setting up the express and the important libraries
+import dbConnect from "./db/db.js";
 import express from "express";
 const app = express();
 import "dotenv/config.js";
-//for info of user accessing request
-import morgan from "morgan";
-//db connection
-import dbConnect from "./db/db.js";
+import cookieParser from "cookie-parser";
+import morgan from "morgan"; //To log every http request on console
+import cors from "cors";
+
+//All the important middlewares that need to be executed first
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json()); //For parsing the content in the req body
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); //For parsing the content in the req cookies
+app.use(morgan("dev"));
+
+//Importing Routes
+
+import hospitalRoutes from "./routes/hospital.routes.js";
+import bedRoutes from "./routes/bedRoutes.js";
+
+//Seting up the routes and subroutes
+
+app.use("/hospitals", hospitalRoutes);
+app.use("/beds", bedRoutes);
+
+//Application Level Route
+app.use("/", (req, res) => {
+  console.log("Application level route is working !");
+  res.send("Hello World");
+});
+
 //Best practice once db is connected now our server is ready to handle the requests
 dbConnect()
   .then(() => {
@@ -17,29 +42,3 @@ dbConnect()
   .catch((err) => {
     console.error(`Failed to connect to DB ${err}`);
   });
-import hospitalRoutes from "./routes/hospital.routes.js";
-import bedRoutes from "./routes/bedRoutes.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-//middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-//Both are important for extracting request body data properly in Express.js.
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-//Routes
-
-//To get the log of each request on the server
-app.use(morgan("dev"));
-app.use("/hospitals", hospitalRoutes);
-app.use("/beds", bedRoutes);
-app.use("/uploads", express.static("uploads"));
-//dumy route
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-// app.listen(process.env.PORT || 3001, () => {
-//   console.log(`Server is running on ${process.env.PORT}`);
-// });
-export default app;
